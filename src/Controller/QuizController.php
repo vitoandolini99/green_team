69b007e55f8d1569f8282e956caaffa9ad315dad
Deniel Stájner <stajner@spsejecna.cz>
2023-02-16 21:39:50 +0100

    quiz somehow works now

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -12,6 +12,26 @@ class QuizController extends AbstractController{
     
     #[Route('/quiz', name: 'quizexample')]
     public function quizexample(): Response {
-        return $this->render('quiz.html.twig');
+        $csvFilePath = $this->getParameter('kernel.project_dir') . '/public/csv/basnickevyrazy.csv';
+        $data = array_map('str_getcsv', file($csvFilePath));
+        array_shift($data);
+        $questions = [];
+        foreach ($data as $row) {
+            if (count($row) >= 5) {
+                $question = [
+                    'text' => $row[0],
+                    'correctAnswer' => $row[1],
+                    'wrongAnswer1' => $row[2],
+                    'wrongAnswer2' => $row[3],
+                    'wrongAnswer3' => $row[4],
+                ];
+                $questions[] = $question;
+            }
+        }
+
+        return $this->render('quiz.html.twig', [
+            'questions' => $questions,
+        ]);
+
     }
 }
