a92549ec3bd870cde2830132dc8ce9d3629278ee
HOME-Alex <vedurnikof@gmail.com>
2023-02-16 22:26:31 +0100

    certificate simple scratch + images folder

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use TCPDF;
 
 class QuizController extends AbstractController{
     
@@ -32,6 +33,46 @@ class QuizController extends AbstractController{
         return $this->render('quiz.html.twig', [
             'questions' => $questions,
         ]);
+    }
 
+    #[Route('/certificate', name: 'certificate')]
+    public function certificateGenerator(): Response {
+        $name = 'John Doe';
+        $percentage = 85;
+
+        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        $pdf->SetCreator('Certificate Generator');
+        $pdf->SetAuthor('John Doe');
+        $pdf->SetTitle('Certificate of Completion');
+        $pdf->SetSubject('Certificate');
+
+        $pdf->SetMargins(0, 30, 0);
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+        $img_file = 'public/images/certificate_background.jpeg';
+        $pdf->Image($img_file, 0, 0, 210, 297, '', '', '', false, 300, '', false, false, 0);
+
+
+        $pdf->AddPage();
+        $pdf->SetFont('helvetica', 'B', 20);
+        $pdf->SetTextColor(0, 0, 0);
+
+        $pdf->Cell(0, 15, 'Certificate of Completion', 0, 1, 'C');
+        $pdf->Ln(10);
+        $pdf->SetFont('helvetica', 'B', 16);
+
+        $pdf->Cell(0, 15, 'This certificate is awarded to ' . $name . ' for successfully completing the course with a score of ' . $percentage . '%', 0, 1, 'C');
+        $pdf->Ln(20);
+        $pdf->SetFont('helvetica', 'B', 14);
+
+        $pdf->Cell(0, 15, '_______________________________', 0, 1, 'C');
+        $pdf->Cell(0, 10, 'Authorized Signature', 0, 1, 'C');
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->setContent($pdf->Output('certificate.pdf', 'S'));
+
+        return $response;
     }
 }
