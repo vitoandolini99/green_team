1d2e904f281b9a9c606c924c175bdfeddcbcbd28
Matěj Josef Koběrský <kobersky@SPSEJECNANET.CZ>
2023-02-17 11:32:04 +0100

    prohlaseni

---
+++ src/Controller/ProhlaseniController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/ProhlaseniController.php
+namespace App\Controller;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProhlaseniController extends AbstractController{
+
+    #[Route('/o-nas', name: 'prohlaseni o pristupnosti')]
+    public function prohlaseni(): Response {
+        return $this->render('about.html.twig');
+    }    
+}
