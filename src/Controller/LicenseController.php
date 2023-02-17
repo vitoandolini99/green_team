aa37bb60451a60f82fc1bcb9af71476c12920e0a
Filip Virva <virva@spsejecna.cz>
2023-02-17 15:14:56 +0100

    added license page and adjusted homepage

---
+++ src/Controller/LicenseController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/ProhlaseniController.php
+namespace App\Controller;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LicenseController extends AbstractController{
+
+    #[Route('/license', name: 'License')]
+    public function license(): Response {
+        return $this->render('license.html.twig');
+    }    
+}
