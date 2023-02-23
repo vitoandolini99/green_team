1086ba4faf6266b68cd5e736cea10748ca7b3bc3
Deniel Stájner <stajner@spsejecna.cz>
2023-02-23 17:02:43 +0100

    i got a glock in my rawrie

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,25 +2,44 @@
 // src/Controller/SecurityController.php
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInteuse;
 use App\Entity\User;
 
 class SecurityController extends AbstractController{
 
-    #[Route('/register', name: 'register')]
-    public function register(Request $request)
-    {
-        $username = $request->request->get('username');
-        $password = $request->request->get('password');
 
-        return $this->redirectToRoute('login');
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
     }
+
+
+    #[Route('/register', name: 'register')]
+    public function reg(UserPasswordHasherInterface $passwordHasher, ManagerRegistry $doctrine, Request $request): Response
+    {
+        if ($request->request->get('username') !== Null && $request->request->get('password') !== Null) {
+            $entityManager = $doctrine->getManager();
+
+            $username = $request->request->get('username');
+            $user = new User();
+            $user->setUsername($username);
+
+            $psswd = $request->request->get('password');;
+            $user->setPassword($psswd);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+        return $this->render('register.html.twig');
+    }
+
 
     #[Route('/login', name: 'login')]
     public function login(Request $request, SessionInterface $session)
@@ -32,7 +51,7 @@ class SecurityController extends AbstractController{
             $username = $request->request->get('username');
             $password = $request->request->get('password');
 
-            $userRepository = $this->getDoctrine()->getRepository(User::class);
+            $userRepository = $this->entityManager->getRepository(User::class);
             $user = $userRepository->findOneBy(['username' => $username]);
 
             if ($user && $passwordEncoder->isPasswordValid($user, $password)) {
